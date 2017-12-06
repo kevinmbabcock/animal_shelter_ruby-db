345182cfe181bfd25694319c427bb169dcf5f88e
Guest User <Guest@epicodus-9.local>
2017-12-06 09:20:40 -0800

    add form for user to add an animal to the database

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ also_reload("lib/**/*.rb")
 require('./lib/animal')
 require('./lib/customer')
 require('pg')
+require('pry')
 
 DB = PG.connect({:dbname => "animal_shelter_test"})
 
@@ -15,4 +16,19 @@ get('/animals') do
   @all_animals = Animal.all
 
   erb(:animals)
+end
+
+get('/add_animal') do
+  erb(:add_animal)
+end
+
+post('/animals') do
+  name = params.fetch("animalName")
+  gender = params.fetch("gender")
+  type = params.fetch("animalType")
+  breed = params.fetch("animalBreed")
+  date_admitted = params.fetch("date_admitted")
+  new_animal = Animal.new({:name => name, :gender => gender, :type => type, :breed => breed, :date_admitted => date_admitted})
+  new_animal.save
+  erb(:success)
 end
